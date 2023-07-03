fe54392a30ca00df140edc6bf443697a6e4b4174
Evan <evanknight18@gmail.com>
2023-07-03 08:25:41 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      backgroundColor: {
+        'black': '#000000',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
+
